096fa4917d921190d29ef393088c29ac5fe4e0a8
evan <epmoring@gmail.com>
2022-07-04 00:26:47 -0700

    Initial Szasz commit

--- albumList.js
+++ albumList.js
@@ -1,15 +1,39 @@
 
 var albumList = [
-    [
-        'Album 1',
-        ['Song 1','Album1/Song1.mp3'],
-        ['Song 2','Album1/Song2.mp3'],
-        ['Song 3','Album1/Song3.mp3']
-    ],
-    [
-        'Album 2',
-        ['Song 1','Album2/Song1.mp3'],
-        ['Song 2','Album2/Song2.mp3'],
-        ['Song 3','Album2/Song3.mp3']
-    ]
-];
+                ['Volume V: Peter The Dolphin',
+
+
+                 ['Polymorph Self','SzaszVol5/PolymorphSelf.mp3'],
+                 ['Quilt Song','SzaszVol5/QuiltSong.mp3'],
+                 ['Midas Song','SzaszVol5/MidasSong.mp3'],
+                 ['Swedish Dogs','SzaszVol5/SwedishDogs.mp3'],
+                ['Another Mortise Song','SzaszVol5/AnotherMortiseSong.mp3'],
+                 ['Abstract Shapes','SzaszVol5/AbstractShapes.mp3'],
+                ['Naming Things','SzaszVol5/NamingThings.mp3']
+                 
+                 ],
+
+		[ 'Volume IV: Szasz Alone',
+                 ['We Raise Lions', '/music/SzaszVol4/01WeRaiseLions.mp3'],
+                 ['Giraffephant', '/music/SzaszVol4/02Giraffephant.mp3'],
+                 ['Almost Perfect Blue', '/music/SzaszVol4/03AlmostPerfectBlue.mp3']],
+		[ 'Volume III: Spectral',
+                 ['Empire', '/music/SzaszVol3/01Empire.mp3'],
+                 ['Gaslight', '/music/SzaszVol3/02Gaslight.mp3'],
+                 ['King Pigeon', '/music/SzaszVol3/03KingPigeon.mp3'],
+		['Mortise Song', '/music/SzaszVol3/04MortiseSong.mp3'],
+                 ['Orlock\'s Choice', '/music/SzaszVol3/05OrlocksChoice.mp3']],
+    
+                [ 'Volume II: Coprolith',
+                 ['Clock Strikes Three', '/music/SzaszVol2/ClockStrikesThree.mp3'],
+                 ['Grecian', '/music/SzaszVol2/Grecian.mp3'],
+                 ['Trainfucker', '/music/SzaszVol2/Trainfucker.mp3'],
+                ['Cell', "/music/SzaszVol2/Cell.mp3"]],
+
+        
+                ['Volume I: Eidos',
+                ['Kids in Sweden',"/music/SzaszVol1/01KidsinSweden.mp3"],
+                ['Shroud of Turin',"/music/SzaszVol1/02ShroudofTurin.mp3"],
+                ['Japanese Professional Wrestler',"/music/SzaszVol1/03JapaneseProfessionalWrestler.mp3"],
+                ['Georgetown',"/music/SzaszVol1/04Georgetown.mp3"]]
+                ];
