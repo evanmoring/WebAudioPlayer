46e3849da291f94c2efff1f26eddbd442e0d103a
evan <epmoring@gmail.com>
2022-07-04 00:04:24 -0700

    Added albumList.js

---
+++ albumList.js
@@ -0,0 +1,15 @@
+
+var albumList = [
+    [
+        'Album 1',
+        ['Song 1','Album1/Song1.mp3'],
+        ['Song 2','Album1/Song2.mp3'],
+        ['Song 3','Album1/Song3.mp3']
+    ],
+    [
+        'Album 2',
+        ['Song 1','Album2/Song1.mp3'],
+        ['Song 2','Album2/Song2.mp3'],
+        ['Song 3','Album2/Song3.mp3']
+    ]
+];
--- music.js
+++ music.js
@@ -1,24 +1,5 @@
 //These arrays generate the song selection section. The format is [[AlbumTitle1,[SongName1,SongPath1],[SongName2,SongPath2]],[AlbumTitle2,[SongName3,SongPath3]]]
 
-var albumList = [
-                ['Album 1',
-
-                
-                 ['Song 1','Album1/Song1.mp3'],
-                 ['Song 2','Album1/Song2.mp3'],
-                 ['Song 3','Album1/Song3.mp3']
-                 
-                 ],
-    
-                ['Album 2',
-
-
-                 ['Song 1','Album2/Song1.mp3'],
-                 ['Song 2','Album2/Song2.mp3'],
-                 ['Song 3','Album2/Song3.mp3']
-                 
-                 ]];
-
 var currentlyPlaying = '';
 var sList = [];
 
