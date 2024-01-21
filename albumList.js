2fdada93edf2c2be689b0834ff126de664aac87c
evan <epmoring@gmail.com>
2024-01-21 15:46:32 -0800

    Release of Language of Man

--- albumList.js
+++ albumList.js
@@ -1,7 +1,7 @@
 
-var albumList = [
+var albumListSzasz = [
                 ['Volume V: Peter The Dolphin',
-
+                  'szasz5.png',
 
                  ['Polymorph Self','SzaszVol5/PolymorphSelf.mp3'],
                  ['Quilt Song','SzaszVol5/QuiltSong.mp3'],
@@ -14,26 +14,44 @@ var albumList = [
                  ],
 
 		[ 'Volume IV: Szasz Alone',
-                 ['We Raise Lions', '/music/SzaszVol4/01WeRaiseLions.mp3'],
-                 ['Giraffephant', '/music/SzaszVol4/02Giraffephant.mp3'],
-                 ['Almost Perfect Blue', '/music/SzaszVol4/03AlmostPerfectBlue.mp3']],
+        'szasz4.png',
+                 ['We Raise Lions', 'SzaszVol4/01WeRaiseLions.mp3'],
+                 ['Giraffephant', 'SzaszVol4/02Giraffephant.mp3'],
+                 ['Almost Perfect Blue', 'SzaszVol4/03AlmostPerfectBlue.mp3']],
 		[ 'Volume III: Spectral',
-                 ['Empire', '/music/SzaszVol3/01Empire.mp3'],
-                 ['Gaslight', '/music/SzaszVol3/02Gaslight.mp3'],
-                 ['King Pigeon', '/music/SzaszVol3/03KingPigeon.mp3'],
-		['Mortise Song', '/music/SzaszVol3/04MortiseSong.mp3'],
-                 ['Orlock\'s Choice', '/music/SzaszVol3/05OrlocksChoice.mp3']],
+        'szasz3.png',
+                 ['Empire', 'SzaszVol3/01Empire.mp3'],
+                 ['Gaslight', 'SzaszVol3/02Gaslight.mp3'],
+                 ['King Pigeon', 'SzaszVol3/03KingPigeon.mp3'],
+		['Mortise Song', 'SzaszVol3/04MortiseSong.mp3'],
+                 ['Orlock\'s Choice', 'SzaszVol3/05OrlocksChoice.mp3']],
     
                 [ 'Volume II: Coprolith',
-                 ['Clock Strikes Three', '/music/SzaszVol2/ClockStrikesThree.mp3'],
-                 ['Grecian', '/music/SzaszVol2/Grecian.mp3'],
-                 ['Trainfucker', '/music/SzaszVol2/Trainfucker.mp3'],
-                ['Cell', "/music/SzaszVol2/Cell.mp3"]],
+                'szasz2.jpg',
+                 ['Clock Strikes Three', 'SzaszVol2/ClockStrikesThree.mp3'],
+                 ['Grecian', 'SzaszVol2/Grecian.mp3'],
+                 ['Trainfucker', 'SzaszVol2/Trainfucker.mp3'],
+                ['Cell', "SzaszVol2/Cell.mp3"]],
 
         
                 ['Volume I: Eidos',
-                ['Kids in Sweden',"/music/SzaszVol1/01KidsinSweden.mp3"],
-                ['Shroud of Turin',"/music/SzaszVol1/02ShroudofTurin.mp3"],
-                ['Japanese Professional Wrestler',"/music/SzaszVol1/03JapaneseProfessionalWrestler.mp3"],
-                ['Georgetown',"/music/SzaszVol1/04Georgetown.mp3"]]
+                'szasz1.jpg',
+                ['Kids in Sweden',"SzaszVol1/01KidsinSweden.mp3"],
+                ['Shroud of Turin',"SzaszVol1/02ShroudofTurin.mp3"],
+                ['Japanese Professional Wrestler',"SzaszVol1/03JapaneseProfessionalWrestler.mp3"],
+                ['Georgetown',"SzaszVol1/04Georgetown.mp3"]]
                 ];
+var albumListCasca = [
+                ['Language of Man',
+                "language_of_man.png",
+
+
+                ['1000 Ships','LanguageOfMan/01_1000_ships.mp3'],
+                ['A Shadow on a Wall','LanguageOfMan/02_a_shadow_on_a_wall.mp3'],
+                ['Orlock\'s Bride','LanguageOfMan/03_orlocks_bride.mp3'],
+                 ['Song for a Bird','LanguageOfMan/04_song_for_a_bird.mp3'],
+                ['Instrumental','LanguageOfMan/05_instrumental.mp3'],
+                ['بنی‌آدم ','LanguageOfMan/06_bani_adam.mp3'],
+                ['Language of Man','LanguageOfMan/07_language_of_man.mp3'],
+
+                 ,]]
--- music.js
+++ music.js
@@ -2,25 +2,58 @@
 
 var currentlyPlaying = '';
 var sList = [];
+var band_descriptions = {
+"Casca":
+`             <br>
+                <span> Eric Balison and I formed Casca and recorded Language of Man in San Francisco in 2023. I play guitar, bass, drums, and sing. Eric sings and plays guitar. We both wrote lyrics.</span>
+                <br>
+                <br>
+                <span>Shane Moring and Agnes Widbom contributed lyrics. Agnes and I did backing vocals. Shirin Makaremi recited the poem  بنی‌آدم (Bani Adam) by the poet سعدی شیرازی, (Saadi Shirazi). Anna Rotty provided the photo for the album artwork. The piece is called Judith Slaying Holofernes. You can see more of her work <a href="https://www.annarotty.com/"> here</a>. Thank you so much for your help!</span>
+                <br><br>
+                <span>Eric's other band is Big Teeth. You can hear their latest recordings, The Babe EP, on bandcamp at this <a href="https://bigteethmusic.bandcamp.com/album/the-babe-ep">link</a></span>
+                <br><br><span>You can download the mp3s in a zip file <a href="LanguageOfMan.zip">here</a></span>
+                `,
+"Szasz":
+`             <br>
+                <span>Andrew Ware and I formed Szasz in 2012. We recorded Volumes I - IV in quick succession in Encinitas, California. I play guitar, bass, keyboards. Andrew plays drums. We both did vocals and recording.</span>
+                <br>
+                <br>
+                <span>For Volume V I took over drums. It was recorded in San Francisco in 2018.</span>
+                <br><br>
+                <span>I'm working on the finishing touches on Volume III and IV so they will but up at some point. If you would like digital versions of the recordings email me and I can give you them.</span>
+                <br><br>
+                <span>Many people have lent us equipment, given us a place to record, or given us technical advice. Special thanks to Diane Baxter, Aaron Dennis, Rene Esteban Franco, Kyle Herbert, John Moring, Mary Jo Preti, Diego Villalobos, Pedro Verdin, and Garth Ware. Also thank you to our biggest fan, Tony Hicks. </span>
+`
+}
+
+function change_band(caller,band,album_list){
+    var band_list = ["szasz_selector", "casca_selector"]
+    for (i in band_list){
+        document.getElementById(band_list[i]).className = ""
+        }
+    caller.className= "chosen_selector"
+    document.getElementById('songs').innerHTML = ""
+    document.getElementById('band_description').innerHTML = band_descriptions[band]
+    albumGen(album_list)
+}
 
 function albumGen(albumList){
     for(var i = 0; i < albumList.length; i++) { 
         current_title = albumList[i][0];
-        current_song_list = albumList[i].slice(1);
-        new Album(current_title, current_song_list);
+        current_album_art = albumList[i][1]
+        current_song_list = albumList[i].slice(2);
+        console.log(current_song_list)
+        new Album(current_title, current_song_list, current_album_art);
     }
 }
     
 class Album {
-    constructor(title, songList){
-        console.log("TITLE")
-        console.log(title)
-        console.log("SONGLIST")
-        console.log(songList)
+    constructor(title, songList, current_album_art){
         this.title = title;
         this.songList = songList;
         this.div = document.createElement("DIV");
         this.div.setAttribute('id',this.title);
+        this.albumArtPath = current_album_art;
         
         document.getElementById('songs').appendChild(this.div);
         var aTitle = document.createElement("P");
@@ -73,7 +106,6 @@ class Song {
         this.audio.addEventListener("ended", () => {
              playNext();
         });
-
     }
 
     play(){
@@ -84,12 +116,14 @@ class Song {
         currentlyPlaying = this; 
         trackingBar(this);
         document.getElementById('audioplayer').style.display="block";
+        document.getElementById('albumArt').src = this.album.albumArtPath
     }
 
     pause(){
       this.audio.pause();
       pButton.className = "pause";
     }
+
     reset(){
       this.audio.pause();
       this.audio.currentTime=0;
@@ -115,7 +149,8 @@ function createAllAudio(songs,album) {
 }
 
 function pauseAll() {
-    for(var i = 1; i < sList.length; i++) { 
+    console.log("PAUSE ALL")
+    for(var i = 0; i < sList.length; i++) { 
       sList[i].reset();
     }  
 }
